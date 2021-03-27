ff6aa37351519f57629a648c717743c1cf3d8931
Ng <yzw@ng.com>
2021-03-27 18:55:08 +0800

    submit

--- example/lib/model/course.dart
+++ example/lib/model/course.dart
@@ -1,3 +1,43 @@
+import 'package:zego_faceunity_plugin_example/tool/function.dart';
+
 class Course {
-  Course.fromJson() {}
+  //  "course_id": 6,//id
+  //     "course_name": "如何养生234",//标题
+  //     "course_type": 0,//课程类型:0视频直播,1音频直播,2视频,3音频,4文章,5线下课
+  //     "pic": "http://rtmpphp.com/data/image/anony/202103/13/a7eae52b269c4b6e.jpg",//封面图
+  //     "course_num": 50,//课时数量(all)
+  //     "course_flag": 0,//课程状态:0连载,1完结
+  //     "is_free": 1,//是否免费0收费,1免费
+  //     "cost": 0,//价格
+  //     "sec": 0//已发布的章节数量
+  //     "single_cost": 0,//单节课时价格 例：￥35/课
+  //     "vip_level": 0,//vip等级 ，
+  //     "desc": "这是一个牛逼的简介哇。。。。。。。。",//课程简介
+  String pic;
+  String name;
+  String desc;
+  int id;
+  int viplevel;
+  int cost;
+  int sec;
+  int singlecost;
+  int type;
+  int flag;
+  int coursenum;
+  int isfree;
+
+  Course.fromJson(data) {
+    this.id = data['course_id'];
+    this.name = data['course_name'];
+    this.type = data['course_type'];
+    this.pic = data['pic'];
+    this.coursenum = data['course_num'];
+    this.flag = data['course_flag'];
+    this.isfree = data['is_free'];
+    this.cost = (data['cost'] / 100).toInt();
+    this.singlecost = (data['single_cost'] / 100).toInt();
+    this.viplevel = data['vip_level'];
+    this.desc = data['desc'];
+    this.sec = data['sec'];
+  }
 }
--- example/lib/sellpage.dart
+++ example/lib/sellpage.dart
@@ -17,15 +17,24 @@ import 'package:zego_faceunity_plugin_example/utils/zego_config.dart';
 import 'tool/image.dart';
 
 class Sellpage extends LoginBase {
-  bool select;
-  String api = 'Course/getLiveClassSeries';
+  //是否选中
+  static bool select = false;
+  //是否编辑中
+  static bool editing = false;
+  //被编辑的课程
+  static Course coursein;
+  static String api = 'Course/getLiveClassSeries';
   //课程列表
-  List courses = [];
-  String cacheindex = 'Sellpage';
-  String cachetime;
+  static List courses = [];
+  static String cacheindex = 'Sellpage';
+  static String cachetime;
 
-  TextEditingController searword = new TextEditingController();
+  static TextEditingController searword = new TextEditingController();
+  static TextEditingController cost = new TextEditingController();
+  static TextEditingController seccost = new TextEditingController();
+  static TextEditingController acttime = new TextEditingController();
   initState() {
+    editing = false;
     cachetime = cacheindex + cacheindex.hashCode.toString();
     //初始化数据
     initcache();
@@ -36,12 +45,11 @@ class Sellpage extends LoginBase {
 //加载数据缓存
   initcache() {
     courses = getcache(cacheindex);
-    // d(courses);
   }
 
   checkoutcache() {
     var cacheout = getcache(cachetime);
-    // d(cacheout);
+
     if (!isnull(cacheout)) {
       httpget();
     }
@@ -67,37 +75,45 @@ class Sellpage extends LoginBase {
     }
   }
 
+  dispose() {
+    // d(1111);
+  }
+
   Sellpage({Key key});
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Container(
-      color: Color.fromRGBO(0, 0, 0, 0.8),
-      padding: EdgeInsets.only(left: 20, right: 20),
-      height: g('h') * 0.5,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          SizedBox(
-            height: 18,
-          ),
-          buildserechbtn(),
-          Expanded(
-              child: Container(
-                  height: 100.0,
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      // scrollDirection: Axis.horizontal,
-                      itemCount: courses.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Column(children: [
-                          beatbtn(index),
-                        ]);
-                      })))
-        ],
-      ),
-    ));
+        child: !editing
+            ? Container(
+                color: Color.fromRGBO(0, 0, 0, 0.8),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: g('h') * 0.5,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      height: 18,
+                    ),
+                    buildserechbtn(),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Expanded(
+                        child: Container(
+                            height: 100.0,
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                // scrollDirection: Axis.horizontal,
+                                itemCount: courses.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Column(children: [
+                                    beatbtn(index),
+                                  ]);
+                                })))
+                  ],
+                ))
+            : edit());
   }
 
   //搜索框栏目
@@ -146,7 +162,7 @@ class Sellpage extends LoginBase {
             ),
             GestureDetector(
               child: Container(
-                  margin: EdgeInsets.only(right: 4),
+                  margin: EdgeInsets.only(right: 14),
                   child: Icon(
                     Icons.close,
                     color: Colors.grey[200],
@@ -161,7 +177,10 @@ class Sellpage extends LoginBase {
     return c;
   }
 
-  _search() {}
+  _search() {
+    d(searword.text);
+  }
+
   _clearword() {
     searword.clear();
     initcache();
@@ -169,42 +188,230 @@ class Sellpage extends LoginBase {
   }
 
   Widget beatbtn(int index) {
+    Course course = Course.fromJson(courses[index]);
+
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Expanded(
-            child: CupertinoButton(
-                borderRadius: BorderRadius.circular(0.0),
+            child: Container(
+                margin: EdgeInsets.only(bottom: 18),
+                padding: EdgeInsets.all(18),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Color(0xff292A33),
+                ),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
 //图片
-                    NgImage(courses[index]['pic']),
+                    NgImage(
+                      course.pic,
+                      width: g('w') * 0.4,
+                      height: g('w') * 0.4 / 1.65,
+                    ),
 //文字
+                    SizedBox(
+                      width: 18,
+                    ),
+                    Expanded(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              course.name,
+                              style: TextStyle(
+                                  color: Color(0xffFFFFFF),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              "" +
+                                  course.sec.toString() +
+                                  "课/共" +
+                                  course.coursenum.toString() +
+                                  "课",
+                              style: TextStyle(
+                                  color: Color(0xffFFFFFF),
+                                  // fontWeight: FontWeight.bold,
+                                  fontSize: 13),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        '￥',
+                                        style: TextStyle(
+                                            color: Color(0xffFF0000),
+                                            // fontWeight: FontWeight.bold,
+                                            fontSize: 8),
+                                      ),
+                                      Text(course.cost.toString(),
+                                          style: TextStyle(
+                                              color: Color(0xffFF0000),
+                                              // fontWeight: FontWeight.bold,
+                                              fontSize: 13)),
+                                    ]),
+                                Expanded(child: SizedBox()),
+                                editbtn(course),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                editbtn(course),
+                              ],
+                            ),
+                          ]),
+                    )
                   ],
                 )))
       ],
     );
-    return CupertinoButton(
-      // padding: const EdgeInsets.all(10.0),
-      pressedOpacity: 1.0,
-      borderRadius: BorderRadius.circular(0.0),
+  }
+
+  gettitle(String title) {
+    TextStyle t1style = TextStyle(fontSize: 16, color: Colors.white);
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      // margin: EdgeInsets.all(20),
+      child: Text(
+        title,
+        style: t1style,
+      ),
+    );
+  }
+
+  getinut(String hint, TextEditingController control) {
+    var inputField = new TextFormField(
+      maxLines: 1,
+      controller: control,
+      onChanged: (str) {
+        // ? haveword = true : haveword = false;
+      },
+      onEditingComplete: () {},
+      decoration: new InputDecoration(
+          contentPadding: EdgeInsets.only(top: -4.0, left: 14, right: 14),
+          hintText: hint,
+          hintStyle: new TextStyle(color: Color(0xff414352)),
+          border: InputBorder.none),
+      style: new TextStyle(fontSize: 15, color: Colors.white),
+      //验证
+      validator: (String value) {
+        return '';
+      },
+      onSaved: (value) {},
+    );
+    return Container(
+      child: inputField,
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+          color: Color(0xff414352), borderRadius: BorderRadius.circular(6.0)),
+    );
+  }
+
+  Widget edit() {
+    return Container(
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Icon(
-            Icons.cast,
-            size: 50.0,
-            color: index == select ? Colors.red : Colors.white,
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                // gettitle('修改直播间优惠价格'),
+                // getinut('请输入价格', cost),
+                gettitle('活动结束时间'),
+                getinut('请输入价格', acttime),
+                gettitle('课时配置'),
+                Container(
+                  height: g('h') * 0.3,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 1,
+                      ),
+                      Column(
+                        children: [
+                          GestureDetector(
+                            child: Text('data'),
+                            onTap: zxf,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          Text(
-            select.toString(),
-            style: TextStyle(color: Colors.white, fontSize: 12),
-          )
+          Row(),
+          Row(),
         ],
       ),
+    );
+  }
+
+  zxf() {
+    selectbox(context, _sex());
+  }
+
+  List<Widget> _sex() {
+    var values = {'1', '2'};
+    var sexname = {'1': lang('男'), '2': lang('女')};
+    return values.map((local) {
+      return ListTile(
+        title: Text(
+          "${sexname[local]}",
+          textAlign: TextAlign.center,
+          // style: TextStyle(
+          //     color: local == sexid ? SQColor.primary : SQColor.darkGray),
+        ),
+        onTap: () {
+          // sexid = local;
+          // sex = sexname[local];
+          // Navigator.pop(context);
+          // if (sexid != user['sex'].toString() && sexid != '0') {
+          //   post = true;
+          // }
+          // if (sexid == '0') {
+          //   post = false;
+          // }
+          reflash();
+        },
+      );
+    }).toList();
+  }
+
+  Widget editbtn(Course course) {
+    return CupertinoButton(
+      padding: const EdgeInsets.all(6.0),
+      minSize: 30,
+      pressedOpacity: 1.0,
+      borderRadius: BorderRadius.circular(6.0),
+      color: Color(0xffFF8A00),
+      child: Text(
+        '编辑',
+        style: TextStyle(color: Colors.white, fontSize: 12),
+      ),
       onPressed: () {
-        setState(() {
-          // beatnselect = index;
-        });
+        coursein = course;
+        editing = true;
+        reflash();
       },
     );
   }
